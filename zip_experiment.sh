2db5df215577c1fcec3b9c61dfe4ffbb9297abb0
TomMladenov <tom.mladenov@ieee.org>
2021-05-14 01:31:55 +0200

    Update package preparation script

--- zip_experiment.sh
+++ zip_experiment.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 version=$(git rev-parse --short HEAD)
+
+if [[ $(git diff --stat) != '' ]]; then
+  version="${version}-dirty"
+else
+  version="${version}-clean"
+fi
+
+
+
+
 package_name=exp145_"$version"_Mladenov.zip
 
-zip -r delivery/$package_name home/*
+#zip -r delivery/$package_name home/*
 
 echo "Finished preparing ZIP file"
 
